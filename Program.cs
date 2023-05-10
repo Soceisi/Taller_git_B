a6abddca44535610e50576ff3c49de7f6f3c17d2
Soceisi <univallesoceisi@gmail.com>
2023-05-10 14:12:30 -0400

    mi primer hola mundo

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace prueba
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            int n1 = 2;
+            int n2 = 3;
+
+            int result = n1 + n2;
+            Console.WriteLine(result);
+        }
+    }
+}
